e0f3c04ed400d8286e10c5c72921ab3f52c109de
Saifbhai90 <ssaifsajid11@gmail.com>
2026-03-22 20:39:20 +0500

    fix: add Alembic migration for emergency_relation column on driver table

---
+++ migrations/versions/h1i2j3k4l5m6_add_emergency_relation_to_driver.py
@@ -0,0 +1,26 @@
+"""add emergency_relation to driver
+
+Revision ID: h1i2j3k4l5m6
+Revises: g9h0i1j2k3l4
+Create Date: 2026-03-22
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'h1i2j3k4l5m6'
+down_revision = 'g9h0i1j2k3l4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('driver', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('emergency_relation', sa.String(length=100), nullable=True))
+
+
+def downgrade():
+    with op.batch_alter_table('driver', schema=None) as batch_op:
+        batch_op.drop_column('emergency_relation')
